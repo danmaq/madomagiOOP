b189869bec69013dc3b0b7e5fe478decc3ba0fa9
Shuhei Nomura <info@danmaq.com>
2017-08-31 00:24:43 +0900

    神は彼女に名前を授け、三名の魔法少女を生み出したが、彼女らには個性がないことに失望した。
    The God gave her a name and created three magical girls. But The God were disappointed that girls didn't have individuality.

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const put = str => $('#list').append(`<li>${str}</li>`);
 
 /** 魔法少女ができること一覧。 */
 class Syojo {
+    constructor(name) {
+        this.name = name;
+    }
     hunt(majo) {
         put('ガッシ！ボカッ！');
         majo.die();
@@ -24,7 +27,11 @@ class Majo {
 /** 準備ができたらここからはじまるよ */
 $(() => {
     put('神は世界を創った');
-    const syojo = new Syojo();
+    const mami = new Syojo('Mami');
+    const homu = new Syojo('HomuHomu');
+    const blossom = new Syojo('Blossom');
     const majo = new Majo();
-    syojo.hunt(majo);
+    mami.hunt(majo);
+    homu.hunt(majo);
+    blossom.hunt(majo);
 });
