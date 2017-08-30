0910ec045dee423148a2a600ec97b579303eb6ab
Shuhei Nomura <info@danmaq.com>
2017-08-31 00:02:13 +0900

    そこで、神は魔女に死ぬ能力を与え、同時に彼女へ魔女を殺すことを教えた。
    So The God gave the witch the skill to die herself, and taught hunter girl to kill the her.

--- main.js
+++ main.js
@@ -9,17 +9,22 @@ const put = str => $('#list').append(`<li>${str}</li>`);
 /** 魔法少女ができること一覧。 */
 class Syojo {
     hunt(majo) {
-        put('魔法少女は魔女を狩った');
+        put('ガッシ！ボカッ！');
+        majo.die();
     }
 }
 
 /** 魔女ができること一覧。 */
-class Majo {}
+class Majo {
+    die() {
+        put('魔女は死んだ');
+    }
+}
 
 /** 準備ができたらここからはじまるよ */
 $(() => {
     put('神は世界を創った');
     const syojo = new Syojo();
     const majo = new Majo();
-    syojo.hunt();
+    syojo.hunt(majo);
 });
