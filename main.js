1e9825db7df82c5c640663306c818677457e28dc
Shuhei Nomura <info@danmaq.com>
2017-08-30 23:42:44 +0900

    神は概念を元に、魔法少女と魔女を世界に産み落とした。
    The God brought down a magical girls and a witch into the world from base already created notion.

--- main.js
+++ main.js
@@ -6,11 +6,15 @@
  */
 const put = str => $('#list').append(`<li>${str}</li>`);
 
+/** 魔法少女ができること一覧。 */
 class Syojo {}
 
+/** 魔女ができること一覧。 */
 class Majo {}
 
 /** 準備ができたらここからはじまるよ */
 $(() => {
     put('神は世界を創った');
+    const syojo = new Syojo();
+    const majo = new Majo();
 });
