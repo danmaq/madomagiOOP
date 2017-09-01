d589ca5f2947c33fab6053e0c9aa2219d050fe56
Shuhei Nomura <info@danmaq.com>
2017-09-01 11:40:39 +0900

    悩んだ神はその赫い眼で、肉体と魂を分離して肉体を使い回すことにした。
    Troubled The God separated their body and soul with red eyes of The God. Girls body decided to swap and use the soul.

--- main.js
+++ main.js
@@ -8,61 +8,61 @@ const put = str => $('#list').append(`<li>${str}</li>`);
 
 /** 魔法少女に共通した特徴一覧。 */
 class Syojo {
-    constructor() {
+    constructor(soul = new Soul()) {
+        this.soul = soul;
         put('<strong>契約が成立し、魔法少女が誕生した</strong>');
     }
     die() {
         put(`${this.constructor.name} は死んだ`);
     }
-    get skill() {
-        return '';
-    }
+    /** ソウルジェムを取得・設定するアクセサ。 */
+    get soul() { return this._soul; }
+    set soul(value) { this._soul = value; }
     hunt(majo) {
-        put(this.skill);
+        put(this.soul.skill);
         majo.die();
     }
 }
 
-/** Mami ができること一覧。 */
-class Mami extends Syojo {
-    get skill() {
-        return 'ティロフィナーレ(笑)';
+/** 空のソウルジェムができること一覧。 */
+class Soul {
+    constructor() {
+        put('<strong>ソウルジェムが創られた</strong>');
     }
+    static get singleton() {
+        return this._singleton || (this._singleton = new this());
+    }
+    get skill() { return ''; }
+}
+
+/** Mami ができること一覧。 */
+class Mami extends Soul {
+    get skill() { return 'ティロフィナーレ(笑)'; }
 }
 
 /** HomuHomu ができること一覧。 */
-class HomuHomu extends Syojo {
-    get skill() {
-        return '時よ！(メガネクイッ)';
-    }
+class HomuHomu extends Soul {
+    get skill() { return '時よ！(メガネクイッ)'; }
 }
 
 /** Blossom ができること一覧。 */
-class Blossom extends Syojo {
-    get skill() {
-        return 'おしりパンチ！';
-    }
+class Blossom extends Soul {
+    get skill() { return 'おしりパンチ！'; }
 }
 
 /** Sayaka ができること一覧。 */
-class Sayaka extends Syojo {
-    get skill() {
-        return '瀟洒なソード投げ攻撃';
-    }
+class Sayaka extends Soul {
+    get skill() { return '瀟洒なソード投げ攻撃'; }
 }
 
 /** Madoka ができること一覧。 */
-class Madoka extends Syojo {
-    get skill() {
-        return 'かみのちから とくと めに やきつけておけ！！';
-    }
+class Madoka extends Soul {
+    get skill() { return 'かみのちから とくと めに やきつけておけ！！'; }
 }
 
 /** Punie ができること一覧。 */
-class Punie extends Syojo {
-    get skill() {
-        return 'プリンセス卍固め!!!!';
-    }
+class Punie extends Soul {
+    get skill() { return 'プリンセス卍固め!!!!'; }
 }
 
 /** 魔女ができること一覧。 */
@@ -82,12 +82,15 @@ class Majo {
 /** 準備ができたらここからはじまるよ */
 $(() => {
     put('神は世界を創った');
+    // 3 人分の肉体を用意する
+    const trio = Array.from({ length: 3 }, () => new Syojo());
     for (let i = 0; i < 10; ++i) {
-        const allstars = [Mami, Madoka, Sayaka, Blossom, HomuHomu, Punie];
-        const trio = allstars // 魔法少女全員を……
+        const allsouls = [Mami, Madoka, Sayaka, Blossom, HomuHomu, Punie];
+        const souls = allsouls // 魔法少女のソウルジェム全員を……
             .sort(() => Math.random() - 0.5) // ランダムに並べ替えて、
-            .filter((v, i) => i < 3) // 最初の3人だけ抜き出して、
-            .map(v => new v()); // この世界に産み落とす！
+            .filter((v, i) => i < trio.length) // 最初の3人だけ抜き出して、
+            .map(v => v.singleton); // ソウルジェム実体を拾う！
+        trio.forEach((v, i) => v.soul = souls[i]); // ソウルジェムを肉体にくっつける
         const majo = new Majo();
         trio.forEach(v => v.hunt(majo)); // 三人の選ばれた少女は、魔女へ一斉攻撃を仕掛ける！
         trio.forEach(majo.attack);
